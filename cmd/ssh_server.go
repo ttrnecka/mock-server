db11dbdd6e9a1ad88abe89f3f99de094be3529e3
Tomas Trnecka <trnecka@gmail.com>
2025-07-20 16:33:08 +0200

    added brocade module

--- cmd/ssh_server.go
+++ cmd/ssh_server.go
@@ -28,6 +28,30 @@ func sshServer(port int) error {
 	return err
 }
 
+// func defaultHandler(s ssh.Session) {
+// 	printPrompt(s)
+// 	scanner := bufio.NewScanner(s)
+// 	scanner.Split(bufio.ScanBytes)
+// 	command := []byte{}
+
+// 	for scanner.Scan() {
+// 		if scanner.Text() == string('\n') || scanner.Text() == string('\r') {
+// 			if string(command) == EXIT_CMD {
+// 				s.Exit(0)
+// 			}
+// 			fmt.Fprintf(s, "\n%s", output(command))
+// 			printPrompt(s)
+// 			command = []byte{}
+// 		} else {
+// 			fmt.Fprint(s, scanner.Text())
+// 			command = append(command, scanner.Bytes()...)
+// 		}
+// 	}
+// 	if err := scanner.Err(); err != nil {
+// 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+// 	}
+// }
+
 func defaultHandler(s ssh.Session) {
 	printPrompt(s)
 	scanner := bufio.NewScanner(s)
@@ -36,12 +60,15 @@ func defaultHandler(s ssh.Session) {
 
 	for scanner.Scan() {
 		if scanner.Text() == string('\n') || scanner.Text() == string('\r') {
+			fmt.Fprint(s, "\n")
 			if string(command) == EXIT_CMD {
 				s.Exit(0)
 			}
-			fmt.Fprintf(s, "\n%s", output(command))
-			printPrompt(s)
-			command = []byte{}
+			result, code := output(command)
+			fmt.Fprintf(s, "%s\n", result)
+			s.Exit(code)
+			// printPrompt(s)
+			// command = []byte{}
 		} else {
 			fmt.Fprint(s, scanner.Text())
 			command = append(command, scanner.Bytes()...)
@@ -56,22 +83,22 @@ func defaultPasswordAuth(ctx ssh.Context, pass string) bool {
 	return pass == DUMMY_PASS
 }
 
-func output(cmd []byte) string {
+func output(cmd []byte) (string, int) {
 	command := strings.ReplaceAll(string(cmd), " ", "_")
 	cmd_file := fmt.Sprintf("%s/%s/commands/%s", MODULES_FOLDER, viper.GetString("module"), command)
 	if _, err := os.Stat(cmd_file); os.IsNotExist(err) {
-		return fmt.Sprintf("%s: command does not exist", cmd)
+		return fmt.Sprintf("%s: command does not exist", cmd), 127
 	}
 
 	b, err := os.ReadFile(cmd_file) // just pass the file name
 	if err != nil {
-		fmt.Print(err)
+		return err.Error(), 1
 	}
-	return string(b)
+	return string(b), 0
 }
 
 func printPrompt(s ssh.Session) {
-	fmt.Fprintf(s, "\n%s ", prompt)
+	fmt.Fprintf(s, "%s ", prompt)
 }
 
 func setPrompt() {
